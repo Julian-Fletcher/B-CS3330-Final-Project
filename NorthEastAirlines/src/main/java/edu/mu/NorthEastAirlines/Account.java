a873d194d6722bc5c9e2adeb9c658f4769920be7
Braedennnnn <149883938+Braedennnnn@users.noreply.github.com>
2024-04-10 15:03:42 -0500

    added accounts

---
+++ NorthEastAirlines/src/main/java/edu/mu/NorthEastAirlines/Account.java
@@ -0,0 +1,12 @@
+package edu.mu.NorthEastAirlines;
+
+public class Account {
+	
+	private String name;
+	private String accountId;
+	private String password;
+	private Flight[] flights;
+	private int points;
+	
+	
+}
