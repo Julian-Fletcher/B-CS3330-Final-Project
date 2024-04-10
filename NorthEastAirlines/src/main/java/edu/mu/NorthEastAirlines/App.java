3e172ded993d7deb5e8a63eaad4f1d7933127f73
Braedennnnn <149883938+Braedennnnn@users.noreply.github.com>
2024-04-10 14:53:08 -0500

    start

---
+++ NorthEastAirlines/src/main/java/edu/mu/NorthEastAirlines/App.java
@@ -0,0 +1,13 @@
+package edu.mu.NorthEastAirlines;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
