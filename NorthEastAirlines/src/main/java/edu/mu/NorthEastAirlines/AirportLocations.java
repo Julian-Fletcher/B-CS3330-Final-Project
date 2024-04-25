321cb46a04e8207685404a9342e1637bba93628c
Ahmad <121472038+ARockEnnin@users.noreply.github.com>
2024-04-25 14:35:51 -0500

    Merge branch 'main' of https://github.com/Julian-Fletcher/B-CS3330-Final-Project.git into main

---
+++ NorthEastAirlines/src/main/java/edu/mu/NorthEastAirlines/AirportLocations.java
@@ -0,0 +1,36 @@
+package edu.mu.NorthEastAirlines;
+
+public enum AirportLocations 
+{
+	NewYorkCity(0),
+	LosAngeles(1),
+	Chicago(2), 
+	Houston(3),
+	Phoenix(4),
+	Philadelphia(5),
+	SanAntonio(6),
+	SanDiego(7),
+	Dallas(8),
+	SanJose(9);
+	
+	final int locationVal;
+	AirportLocations(int locationVal) 
+	{
+		this.locationVal = locationVal;
+	}
+	public int getLocationVal()
+	{
+		return locationVal;
+	}
+	public static AirportLocations getAirportLocations(int value)
+	{
+		for(AirportLocations locations : AirportLocations.values())
+		{
+			if(locations.getLocationVal() == value)
+			{
+				return locations;
+			}
+		}
+		return null;
+	}
+}
--- NorthEastAirlines/src/main/java/edu/mu/NorthEastAirlines/FlightManagement.java
+++ NorthEastAirlines/src/main/java/edu/mu/NorthEastAirlines/FlightManagement.java
@@ -1,6 +1,50 @@
 package edu.mu.NorthEastAirlines;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Iterator;
+import java.util.Random;
+
 public class FlightManagement 
 {
+	public void generateRandomFlights()
+	{
+		Flight flight = new Flight();
+		Airport arriveAirport = new Airport();
+		Airport departAirport = new Airport();
+		LocalDateTime departTime;
+		LocalDateTime arriveTime;
+		int flightNumber;
+		PlaneObject plane = new PlaneObject();
+		AirportLocations arriveLocal = AirportLocations.getAirportLocations(randomAirportLocation());
+		AirportLocations departLocal = AirportLocations.getAirportLocations(randomAirportLocation());
+		
+		while(arriveLocal == departLocal)
+		{
+			departLocal = AirportLocations.getAirportLocations(randomAirportLocation());
+		}
+		arriveAirport.airportCode = randomAirportCode();
+		arriveAirport.city = arriveLocal.toString();
+		departAirport.airportCode = randomAirportCode();
+		departAirport.city = departLocal.toString();
+	}
 	
+	public String randomAirportCode()
+	{
+		Random random = new Random();
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 4; i++)
+		{
+			char randomChar = (char)(random.nextInt(26) + 'A');
+			sb.append(randomChar);
+		}
+		return sb.toString();
+	}
+	
+	public int randomAirportLocation()
+	{
+		int maxLocations = 10;
+		Random random = new Random();
+		return random.nextInt(maxLocations);
+	}
 }
