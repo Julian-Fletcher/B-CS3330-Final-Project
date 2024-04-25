5e35ddac9c6e3d3f33d98a592d780117e56b00ef
Ahmad <121472038+ARockEnnin@users.noreply.github.com>
2024-04-25 15:05:14 -0500

    Working on a robust random flight generator

--- NorthEastAirlines/src/main/java/edu/mu/NorthEastAirlines/FlightManagement.java
+++ NorthEastAirlines/src/main/java/edu/mu/NorthEastAirlines/FlightManagement.java
@@ -2,6 +2,7 @@ package edu.mu.NorthEastAirlines;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Iterator;
 import java.util.Random;
 
@@ -14,7 +15,7 @@ public class FlightManagement
 		Airport departAirport = new Airport();
 		LocalDateTime departTime;
 		LocalDateTime arriveTime;
-		int flightNumber;
+		int flightNumber = -1;
 		PlaneObject plane = new PlaneObject();
 		AirportLocations arriveLocal = AirportLocations.getAirportLocations(randomAirportLocation());
 		AirportLocations departLocal = AirportLocations.getAirportLocations(randomAirportLocation());
@@ -23,10 +24,14 @@ public class FlightManagement
 		{
 			departLocal = AirportLocations.getAirportLocations(randomAirportLocation());
 		}
+		
 		arriveAirport.airportCode = randomAirportCode();
 		arriveAirport.city = arriveLocal.toString();
 		departAirport.airportCode = randomAirportCode();
 		departAirport.city = departLocal.toString();
+		flightNumber = randomFlightNumber();
+		plane.setModel(PlaneType.gePlaneType(randomPlaneType()));
+		
 	}
 	
 	public String randomAirportCode()
@@ -46,5 +51,46 @@ public class FlightManagement
 		int maxLocations = 10;
 		Random random = new Random();
 		return random.nextInt(maxLocations);
+	}
+	
+	public int randomFlightNumber()
+	{
+		Random random = new Random();
+		int id = random.nextInt(9000) + 1000;
+		return id;
+	}
+	
+	public int randomPlaneType()
+	{
+		Random random = new Random();
+		return random.nextInt(3);
+	}
+	
+	public LocalDateTime randomArrivaLocalDateTime()
+	{
+		 Random random = new Random();
+	     int year1 = random.nextInt(2025 - 2000) + 2000;
+	     int month1 = random.nextInt(12) + 1;
+	     Month monthEnum1 = Month.of(month1);
+	     int day1 = random.nextInt(monthEnum1.length(year1 % 4 == 0)) + 1;
+	     int hour1 = random.nextInt(24);
+	     int minute1 = random.nextInt(60);
+	     int second1 = random.nextInt(60);
+	     LocalDateTime arrivaLocalDateTime = LocalDateTime.of(year1, month1, day1, hour1, minute1, second1);
+	     return arrivaLocalDateTime;
+	}
+	
+	public LocalDateTime randomDeparLocalDateTime()
+	{
+		Random random = new Random();
+	     int year1 = random.nextInt(2025 - 2000) + 2000;
+	     int month1 = random.nextInt(12) + 1;
+	     Month monthEnum1 = Month.of(month1);
+	     int day1 = random.nextInt(monthEnum1.length(year1 % 4 == 0)) + 1;
+	     int hour1 = random.nextInt(24);
+	     int minute1 = random.nextInt(60);
+	     int second1 = random.nextInt(60);
+	     LocalDateTime deparLocalDateTime = LocalDateTime.of(year1, month1, day1, hour1, minute1, second1);
+	     return deparLocalDateTime;
 	}
 }
--- NorthEastAirlines/src/main/java/edu/mu/NorthEastAirlines/PlaneType.java
+++ NorthEastAirlines/src/main/java/edu/mu/NorthEastAirlines/PlaneType.java
@@ -1,7 +1,28 @@
 package edu.mu.NorthEastAirlines;
 
 public enum PlaneType {
-	LARGE_PLANE,
-	MEDIUM_PLANE,
-	SMALL_PLANE
+	LARGE_PLANE(0),
+	MEDIUM_PLANE(1),
+	SMALL_PLANE(2);
+	
+	final int planeVal;
+	PlaneType(int planeVal) 
+	{
+		this.planeVal = planeVal;
+	}
+	public int getPlaneVal()
+	{
+		return planeVal;
+	}
+	public static PlaneType gePlaneType(int planeVal)
+	{
+		for(PlaneType planes : PlaneType.values())
+		{
+			if(planes.getPlaneVal() == planeVal)
+			{
+				return planes;
+			}
+		}
+		return null;
+	}
 }
